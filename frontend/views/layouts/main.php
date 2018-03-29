e387333d316f81c8f83dc73c897fbb193fe730d8
dzyof <dzyof1987@gmail.com>
2018-03-29 12:59:19 +0300

    first commit in personal cab

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -71,6 +71,8 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
+
+    
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
